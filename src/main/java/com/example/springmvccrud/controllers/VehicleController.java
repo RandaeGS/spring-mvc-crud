d61fabd7897c1926e45c10a9e4c64e676caac571
RandaeGS <garciasanchezrd@gmail.com>
2024-12-14 22:43:18 -0400

    init controller

---
+++ src/main/java/com/example/springmvccrud/controllers/VehicleController.java
@@ -0,0 +1,21 @@
+package com.example.springmvccrud.controllers;
+
+import com.example.springmvccrud.encapsulations.Vehicle;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class VehicleController {
+
+	List<Vehicle> vehicleList = new ArrayList<>();
+
+	@GetMapping("/")
+	public String mainPage(Model model){
+
+		return "";
+	}
+}
