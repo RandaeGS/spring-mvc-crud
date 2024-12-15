5baddd48b1f25f7c021df91da80ed00a35b82d61
RandaeGS <garciasanchezrd@gmail.com>
2024-12-14 21:11:04 -0400

    init vehicle class

---
+++ src/main/java/com/example/springmvccrud/encapsulations/Vehicle.java
@@ -0,0 +1,11 @@
+package com.example.springmvccrud.encapsulations;
+
+public class Vehicle {
+	private long id;
+	private String brand;
+	private String model;
+	private String type;
+	private int fabricationYear;
+	private String color;
+	private double mileage;
+}
