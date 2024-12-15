9d828a22ae9cf40fc0a729550a5e3918a351c45d
RandaeGS <garciasanchezrd@gmail.com>
2024-12-14 22:27:11 -0400

    add constructors and logic to vehicle class

--- src/main/java/com/example/springmvccrud/encapsulations/Vehicle.java
+++ src/main/java/com/example/springmvccrud/encapsulations/Vehicle.java
@@ -1,11 +1,17 @@
 package com.example.springmvccrud.encapsulations;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @NoArgsConstructor @AllArgsConstructor
 public class Vehicle {
 	private long id;
 	private String brand;
 	private String model;
 	private String type;
 	private int fabricationYear;
-	private String color;
-	private double mileage;
+	private @Setter String color;
+	private @Setter double mileage;
 }
