b2924e88e81d391975b1af40e090779c6a86b69a
RandaeGS <garciasanchezrd@gmail.com>
2024-12-21 19:30:59 -0400

    create vehicle service and controller

--- src/main/java/com/example/springmvccrud/controller/VehicleController.java
+++ src/main/java/com/example/springmvccrud/controller/VehicleController.java
@@ -1,12 +1,8 @@
-package com.example.springmvccrud.controllers;
+package com.example.springmvccrud.controller;
 
-import com.example.springmvccrud.encapsulations.Vehicle;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @Controller
 public class VehicleController {
---
+++ src/main/java/com/example/springmvccrud/service/VehicleService.java
@@ -0,0 +1,7 @@
+package com.example.springmvccrud.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class VehicleService {
+}
