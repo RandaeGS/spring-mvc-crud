183d635a78a0d367ae79a93ab45cd06d25448cec
RandaeGS <garciasanchezrd@gmail.com>
2024-12-21 20:47:19 -0400

    remove lombok from vehicle class

--- src/main/java/com/example/springmvccrud/domain/entity/Vehicle.java
+++ src/main/java/com/example/springmvccrud/domain/entity/Vehicle.java
@@ -4,15 +4,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-@Builder
 @Entity
 public class Vehicle {
 	@Id
@@ -24,4 +16,68 @@ public class Vehicle {
 	private int fabricationYear;
 	private String color;
 	private double mileage;
+
+	public Vehicle() {
+	}
+
+	public Vehicle(String brand, String model, String type, int fabricationYear, String color, double mileage) {
+		this.brand = brand;
+		this.model = model;
+		this.type = type;
+		this.fabricationYear = fabricationYear;
+		this.color = color;
+		this.mileage = mileage;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public int getFabricationYear() {
+		return fabricationYear;
+	}
+
+	public void setFabricationYear(int fabricationYear) {
+		this.fabricationYear = fabricationYear;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+
+	public double getMileage() {
+		return mileage;
+	}
+
+	public void setMileage(double mileage) {
+		this.mileage = mileage;
+	}
 }
