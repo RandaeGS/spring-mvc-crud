3ec5369fb2f3ac653f29a8b7a272aea56ead9481
RandaeGS <garciasanchezrd@gmail.com>
2024-12-21 20:11:00 -0400

    list vehicles

--- src/main/java/com/example/springmvccrud/controller/VehicleController.java
+++ src/main/java/com/example/springmvccrud/controller/VehicleController.java
@@ -1,5 +1,7 @@
 package com.example.springmvccrud.controller;
 
+import com.example.springmvccrud.service.VehicleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,8 +9,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class VehicleController {
 
+	@Autowired
+	private VehicleService vehicleService;
+
 	@GetMapping("/")
 	public String mainPage(Model model){
+		model.addAttribute("vehicles", vehicleService.listVehicles());
 		return "mainPage";
 	}
 }
--- src/main/java/com/example/springmvccrud/service/VehicleService.java
+++ src/main/java/com/example/springmvccrud/service/VehicleService.java
@@ -1,7 +1,21 @@
 package com.example.springmvccrud.service;
 
+import com.example.springmvccrud.domain.entity.Vehicle;
+import com.example.springmvccrud.domain.repository.VehicleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class VehicleService {
+
+	@Autowired
+	private VehicleRepository vehicleRepository;
+
+	public List<Vehicle> listVehicles(){
+		return vehicleRepository.findAll();
+	}
+
 }
